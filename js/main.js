4f5b148987ef04cacb97fb2e2b0951e358d9a851
Kirill <efremenkov1999@gmail.com>
2020-10-30 19:19:04 +0300

    add restaurant cards

--- js/main.js
+++ js/main.js
@@ -19,8 +19,20 @@ const cardsMenu = document.querySelector('.cards-menu');
 
 let login = localStorage.getItem('kiruhaDelivery');
 
+const getData = async function(url) {
+
+  const response = await fetch(url);
+
+  if(!response.ok) {
+    throw new Error(`Ошибка по адресу ${url}, статус ошибка ${response.status}!`);
+  }
+
+  return await response.json();
+};
+
 function toogleModalAuth() {
   modalAuth.classList.toggle('is-open');
+  loginInput.style.borderColor = '';
 }
 
 function toggleModal() {
@@ -55,21 +67,29 @@ function notAutorized(){
 
   function logIn(event) {
     event.preventDefault();
-    login = loginInput.value;
-
-    localStorage.setItem('kiruhaDelivery', login);
-
-    toogleModalAuth();
-    buttonAuth.removeEventListener('click', toogleModalAuth);
-    closeAuth.removeEventListener('click', toogleModalAuth);
-    logInForm.removeEventListener('submit', logIn);
-    logInForm.reset();
-    checkAuth();
+    if(loginInput.value.trim()){
+      login = loginInput.value;
+      localStorage.setItem('kiruhaDelivery', login);
+      toogleModalAuth();
+      buttonAuth.removeEventListener('click', toogleModalAuth);
+      closeAuth.removeEventListener('click', toogleModalAuth);
+      logInForm.removeEventListener('submit', logIn);
+      logInForm.reset();
+      checkAuth();
+    } else{
+      loginInput.style.borderColor = '#ff0000';
+      loginInput.value = '';
+    }
   }
 
   buttonAuth.addEventListener('click', toogleModalAuth);
   closeAuth.addEventListener('click', toogleModalAuth);
   logInForm.addEventListener('submit', logIn);
+  modalAuth.addEventListener('click', function(event) {
+    if(event.target.classList.contains('is-open')) {
+      toogleModalAuth();
+    }
+  })
 }
 
 function checkAuth(){
@@ -80,22 +100,34 @@ function checkAuth(){
   }
 }
 
-function createCardRestaurants(){
+function createCardRestaurants(restaurant){
+
+  console.log(restaurant);
+  
+  const {
+    image, 
+    kitchen,
+    name,
+    price,
+    stars,
+    products,
+    time_of_delivery: timeOfDelivery
+  } = restaurant;
 
   const card = `
-    <a class="card card-restaurant">
-      <img src="img/tanuki/preview.jpg" alt="image" class="card-image"/>
+    <a class="card card-restaurant" data-products="${products}">
+      <img src="${image}" alt="image" class="card-image"/>
       <div class="card-text">
         <div class="card-heading">
-          <h3 class="card-title">Тануки</h3>
-          <span class="card-tag tag">60 мин</span>
+          <h3 class="card-title">${name}</h3>
+          <span class="card-tag tag">${timeOfDelivery} мин</span>
         </div>
         <div class="card-info">
           <div class="rating">
-            4.5
+            ${stars}
           </div>
-          <div class="price">От 1 200 ₽</div>
-          <div class="category">Суши, роллы</div>
+          <div class="price">От ${price} ₽</div>
+          <div class="category">${kitchen}</div>
         </div>
       </div>
     </a>
@@ -105,19 +137,27 @@ function createCardRestaurants(){
 
 }
 
-function createCardGood() {
+function createCardGood(goods) {
+
+  const {
+    description,
+    id,
+    image,
+    name,
+    price
+  } = goods;
+
   const card = document.createElement('div');
   card.className = 'card';
 
   card.innerHTML = `
-						<img src="img/pizza-plus/pizza-vesuvius.jpg" alt="image" class="card-image"/>
+						<img src="${image}" alt="image" class="card-image"/>
 						<div class="card-text">
 							<div class="card-heading">
-								<h3 class="card-title card-title-reg">Пицца Везувий</h3>
+								<h3 class="card-title card-title-reg">${name}</h3>
 							</div>
 							<div class="card-info">
-								<div class="ingredients">Соус томатный, сыр «Моцарелла», ветчина, пепперони, перец
-									«Халапенье», соус «Тобаско», томаты.
+								<div class="ingredients">${description}
 								</div>
 							</div>
 							<div class="card-buttons">
@@ -125,7 +165,7 @@ function createCardGood() {
 									<span class="button-card-text">В корзину</span>
 									<span class="button-cart-svg"></span>
 								</button>
-								<strong class="card-price-bold">545 ₽</strong>
+								<strong class="card-price-bold">${price} ₽</strong>
 							</div>
 						</div>
   `
@@ -136,35 +176,51 @@ function createCardGood() {
 
 function openGoods(event){
   const target = event.target;
-  const restaurant = target.closest('.card-restaurant');
+  if(login) {
+    const restaurant = target.closest('.card-restaurant');
 
-  if(restaurant) {
-    containerPromo.classList.add('hide');
-    restaurants.classList.add('hide');
-    menu.classList.remove('hide');
-
-    cardsMenu.textContent = '';
-    
-    createCardGood();
-    createCardGood();
-    createCardGood();
-
+    if(restaurant){
+      cardsMenu.textContent = '';
+      containerPromo.classList.add('hide');
+      restaurants.classList.add('hide');
+      menu.classList.remove('hide');
+      getData(`./db/${restaurant.dataset.products}`).then(function(data){
+        data.forEach(createCardGood);
+      });
+      
+    }else{
+      toogleModalAuth();
+    }
   }
 }
 
-cartButton.addEventListener("click", toggleModal);
+function init(){
+  getData('./db/partners.json').then(function(data){
+    data.forEach(createCardRestaurants);
+  });
+  
+  cartButton.addEventListener("click", toggleModal);
+  
+  close.addEventListener("click", toggleModal);
+  
+  cardsRestaurants.addEventListener('click', openGoods);
+  
+  logo.addEventListener('click', function(){
+      containerPromo.classList.remove('hide');
+      restaurants.classList.remove('hide');
+      menu.classList.add('hide');
+  })
+  
+  checkAuth();
+  
+  new Swiper('.swiper-container',{
+    loop:true,
+    sliderPerWiev:1,
+  });
+}
 
-close.addEventListener("click", toggleModal);
+init();
 
-cardsRestaurants.addEventListener('click', openGoods);
-
-logo.addEventListener('click', function(){
-    containerPromo.classList.remove('hide');
-    restaurants.classList.remove('hide');
-    menu.classList.add('hide');
-})
-
-checkAuth();
-
+createCardRestaurants();
 createCardRestaurants();
 createCardRestaurants();
